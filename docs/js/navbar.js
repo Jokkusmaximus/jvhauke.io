36635792aede7a87739b16afc71a737644ef1e17
Joakim VH <jv.haukedal@tum.de>
2026-04-29 17:08:49 +0200

    Created Projects page layout

--- docs/js/navbar.js
+++ docs/js/navbar.js
@@ -7,8 +7,9 @@ document.getElementById('navbar-placeholder').innerHTML = `
     <div class="site-header-name">Joakim Vik Haukedal</div>
     <nav class="site-nav">
       <a href="index.html">Home</a>
-      <a href="https://github.com/Jokkusmaximus" target="_blank" rel="noopener noreferrer">GitHub</a>
       <a href="joakim_vh_cv.html">My CV</a>
+      <a href="projects.html">Projects</a>
+      <a href="https://github.com/Jokkusmaximus" target="_blank" rel="noopener noreferrer">GitHub</a>
     </nav>
   </header>
 `;
